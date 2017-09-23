797e331a8f0b3c590a048566bb7f902d95c30896
Amir Harel <harel.amir1@gmail.com>
2017-09-23 23:38:48 +0200

    removed comment

--- src/server/api/index.js
+++ src/server/api/index.js
@@ -3,7 +3,6 @@ const packageInfo = require('../../../package.json');
 
 const router = express.Router();
 
-// we might take the profile id from a session or parameter
 router.get('/version', (req, res) => {
   res.json({
     version: packageInfo.version,
