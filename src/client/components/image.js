dcc9432d19b289c7bf5fb47e35e6322bfb63b950
Amir  Harel <amiharel@team.mobile.de>
2017-09-27 09:12:15 +0200

    simple dnd (not working on android)

---
+++ src/client/components/image.js
@@ -0,0 +1,181 @@
+import React from 'react';
+import styled from 'styled-components';
+import { DragSource, DropTarget } from "react-dnd";
+import { findDOMNode } from 'react-dom';
+
+const ImageWrapper = styled.div`
+  position: relative; 
+  display: inline-block; 
+  user-select: none;
+  transition: transform 0.5s;
+  
+  &.touch {
+    transform: scale(1.2);
+  }
+  
+  img {
+    width: 100%;
+    height: 100%;
+  }
+  
+  
+`;
+
+const Overlay = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0,0,0,0.3);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+
+class Image extends React.Component {
+  state = {
+    touching: false,
+  };
+
+  lastTouch = null;
+
+  onTouchStart = () => {
+    console.log('onTouchStart')
+    this.timer = setTimeout(() => {
+      this.setState({ touching: true });
+    }, 500);
+
+  };
+  onTouchMove = () => {
+    console.log('onTouchMove')
+    if (this.timer) {
+      clearTimeout(this.timer);
+      this.timer = null;
+    }
+    this.setState({ touching: false });
+  };
+  onTouchEnd = () => {
+    console.log('onTouchEnd')
+    if (this.timer) {
+      clearTimeout(this.timer);
+      this.timer = null;
+    }
+    this.setState({ touching: false });
+  };
+
+  render() {
+    const { src, connectDragSource, connectDropTarget, isDragging } = this.props;
+    const opacity = isDragging ? 0 : 1;
+
+
+    return connectDragSource(connectDropTarget(
+      <div style={{ opacity }} onTouchStart={this.onTouchStart} onTouchMove={this.onTouchMove} onTouchEnd={this.onTouchEnd} className="wrapper">
+        <ImageWrapper className={`image ${this.state.touching ? "touch": ""}`}>
+          <Overlay>Overlay</Overlay>
+          <img src={src} />
+        </ImageWrapper>
+      </div>
+    ));
+  }
+}
+
+const cardSource = {
+  beginDrag(props) {
+    console.log("begin drag.....");
+
+    return {
+      id: props.id,
+      index: props.index,
+    };
+  },
+
+  endDrag() {
+
+  }
+  //
+  // canDrag(props) {
+  //   console.log("candrag", props);
+  //   return false;
+  // }
+};
+
+const cardTarget = {
+  drop(props, monitor, component) {
+    // const dragIndex = monitor.getItem().index;
+    // const hoverIndex = props.index;
+
+    // Don't replace items with themselves
+    // if (dragIndex === hoverIndex) {
+    //     return;
+    // }
+
+    // Time to actually perform the action
+    // props.moveCard(dragIndex, hoverIndex);
+
+    // Note: we're mutating the monitor item here!
+    // Generally it's better to avoid mutations,
+    // but it's good here for the sake of performance
+    // to avoid expensive index searches.
+    // monitor.getItem().index = hoverIndex;
+  },
+  hover(props, monitor, component) {
+    const dragIndex = monitor.getItem().index;
+    const hoverIndex = props.index;
+
+    // Don't replace items with themselves
+    if (dragIndex === hoverIndex) {
+      return;
+    }
+
+    // Determine rectangle on screen
+   // const hoverBoundingRect = findDOMNode(component).getBoundingClientRect();
+
+    // Get vertical middle
+    //const hoverMiddleY = (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
+
+    // Determine mouse position
+    //const clientOffset = monitor.getClientOffset();
+
+    // Get pixels to the top
+   // const hoverClientY = clientOffset.y - hoverBoundingRect.top;
+
+    // Only perform the move when the mouse has crossed half of the items height
+    // When dragging downwards, only move when the cursor is below 50%
+    // When dragging upwards, only move when the cursor is above 50%
+
+    // Dragging downwards
+   // if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+   ///   return;
+   // }
+
+    // Dragging upwards
+  //  if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+   //   return;
+  //  }
+
+    if (!props.onSwap) return;
+
+    // Time to actually perform the action
+    props.onSwap(dragIndex, hoverIndex);
+
+    // Note: we're mutating the monitor item here!
+    // Generally it's better to avoid mutations,
+    // but it's good here for the sake of performance
+    // to avoid expensive index searches.
+    monitor.getItem().index = hoverIndex;
+  },
+};
+
+const droppableComponent = DropTarget("IMAGE", cardTarget,  (connect, monitor) => ({
+  connectDropTarget: connect.dropTarget(),
+  isOver: monitor.isOver(),
+}))(Image);
+
+const draggableComponent = DragSource("IMAGE", cardSource, (connect, monitor) => ({
+  connectDragSource: connect.dragSource(),
+  isDragging: monitor.isDragging(),
+}))(droppableComponent);
+
+export default draggableComponent;
--- src/client/pages/home/index.js
+++ src/client/pages/home/index.js
@@ -2,37 +2,88 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import styled from 'styled-components';
+import HTML5Backend from 'react-dnd-html5-backend';
+import TouchBackend from 'react-dnd-touch-backend';
+import MultiBackend, { TouchTransition, Preview } from 'react-dnd-multi-backend';
+import { DragDropContext } from 'react-dnd';
 import { COLORS } from '../../styles/constants';
-
-const HomeTitle = styled.h1`
-  text-transform: capitalize;
-  color: ${COLORS.TEXT_GRAY_FOCUS};
-`;
+import Image from '../../components/image';
 
 const Content = styled.div``;
 
+
+const IMAGES = [
+  { id: 0, src: "http://all4desktop.com/data_images/1280%20x%20800/4198671-green-sea-view.jpg" },
+  { id: 1, src: "http://www.eagleview.com.au/_assets/img/testimonials/Testimonial-3.jpg" },
+  { id: 2, src: "http://i.dailymail.co.uk/i/pix/2017/04/11/06/3F23D80300000578-4399486-Palace_of_Westminster_view_from_the_Westminster_Bridge_One_of_th-a-12_1491890238800.jpg" },
+  { id: 3, src: "https://assets.kpmg.com/content/dam/kpmg/images/2015/11/view-from-the-top-v2.jpg/jcr:content/renditions/cq5dam.web.512.341.jpg" },
+  { id: 4, src: "http://www.westerntourshuahin.com/image/upload/Lake%20View-huahin%20(8).jpg" },
+];
+
+const BigContainer = styled.div`
+  
+  .image {
+    width: 100%;
+    height: 100%;
+  }
+  
+  
+`;
+
+const SmallContainer = styled.div`
+  .wrapper {
+    display: inline-block;
+  }
+  
+  .image {
+    width: 90px;
+    height: 90px;
+    margin: 10px;
+  }
+`;
+
+const Long = styled.div`
+  background-color: #efefef;
+  height: 700px;
+`;
+
 class Home extends React.Component {
   state = {
-    color: 'red',
+    images: IMAGES,
   };
 
-  onTitleClick = () => {
-    this.setState({ color: this.state.color === 'red' ? 'green' : 'red' });
+
+  generatePreview = (type, item, style) => {
+    console.log("generatePreview", type, item, style);
+    return (
+      <SmallContainer style={{...style, width: 90, height: 90, zIndex: 99999}} className="test" >
+        <Image src={this.state.images.find(data => data.id === item.id).src}/>
+      </SmallContainer>
+    );
+  };
+
+  changeImageLocation = (dragIndex, hoverIndex) => {
+    console.log("changeImageLocation:",dragIndex, hoverIndex );
+    this.setState({ images: this.state.images.map((image, index) => {
+      if (index === dragIndex) return this.state.images[hoverIndex];
+      if (index === hoverIndex) return this.state.images[dragIndex];
+      return image;
+    }) });
   };
 
   render() {
     const { ready } = this.props;
-
+    const { images } = this.state;
     return (
       <Content>
-        <HomeTitle
-          className={ready ? 'ready' : 'not-ready'}
-          onClick={this.onTitleClick}
-          style={{ color: this.state.color }}
-        >
-          Start dev from this place...
-        </HomeTitle>
-        <img alt="" src="/static/react-logo.png" />
+        <Preview generator={this.generatePreview} />
+        <BigContainer>
+          <Image onSwap={this.changeImageLocation} src={images[0].src} id={images[0].id} index={0}/>
+        </BigContainer>
+        <SmallContainer>
+          {images.map((data, index) => index === 0 ? null : <Image onSwap={this.changeImageLocation}  key={data.id} index={index} id={data.id} src={data.src} />)}
+        </SmallContainer>
+        <Long>kjdfhg dfkjgh dfskgjh fdslkgjh dsfklgjh sdfkjlgh </Long>
       </Content>
     );
   }
@@ -54,4 +105,19 @@ Home.propTypes = {
   ready: PropTypes.bool.isRequired,
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Home);
+const HTML5toTouch = {
+  backends: [
+    {
+      backend: HTML5Backend
+    },
+    {
+      backend: TouchBackend({enableMouseEvents: true, delayTouchStart: 500}), // Note that you can call your backends with options
+      preview: true,
+      transition: TouchTransition
+    }
+  ]
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(DragDropContext(MultiBackend(HTML5toTouch))(Home));
+
+//export default connect(mapStateToProps, mapDispatchToProps)(Home);
